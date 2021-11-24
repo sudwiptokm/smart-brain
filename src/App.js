6f7699cfda234edc350f25edbda2e073547c8d9e
sudwiptokm <94325115+sudwiptokm@users.noreply.github.com>
2021-11-25 04:49:42 +0600

    Upto face detection part

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition"
+import Clarifai from "clarifai"
+
+const app =new Clarifai.App({
+  apiKey: "0f0c3e5c71af4fa2b8c22bc5a2806217"
+})
 
 const particleOptions = {
-  "particles": {
-      "number": {
-          "value": 50,
-          "density":{
-            "enable" : true,
-            "value_area": 800
-          }
-      },
-      "size": {
-          "value": 3
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
       }
+    }
   },
-  "interactivity": {
-      "events": {
-          "onhover": {
-              "enable": true,
-              "mode": "repulse"
+  interactivity: {
+      events: {
+          onhover: {
+              enable: true,
+              mode: "repulse"
           }
       }
   }
@@ -31,6 +34,37 @@ const particleOptions = {
 
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      input: "",
+      imageUrl: "", 
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage")
+    const height = Number(image.height)
+    const width = Number(image.width)
+    console.log(width,height)
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then( response => this.calculateFaceLocation(response))
+      .catch (err => console.log(err))
+  }
+
   render(){
     return(
       <div className="App">
@@ -38,8 +72,8 @@ class App extends Component {
         <Navigaton />
         <Logo />
         <Rank/>
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm onInputChange = {this.onInputChange} onSubmit = {this.onSubmit}/>
+        <FaceRecognition imageUrl = {this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ImageLinkForm.css"
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onSubmit}) => {
     return(
         <div>
             <p className = "white b f2 ">{"Please Upload your file to detect faces"}</p>
             <div className="center pa4 br3 shadow-5 form">
-                <input type = "text" placeholder = "Your Image Link" className = "w-70 f4 pa2 center br3 bn mr2"></input>
-                <button className="grow w-30 f4 link ph3 pv2 dib black bg-light-blue br3 bn ml2">Detect</button>
+                <input type = "text" placeholder = "Your Image Link" className = "w-70 f4 pa2 center br3 bn mr2" onChange = {onInputChange}></input>
+                <button className="grow w-30 f4 link ph3 pv2 dib black bg-light-blue br3 bn ml2" onClick = {onSubmit}>Detect</button>
             </div>            
         </div>
         
