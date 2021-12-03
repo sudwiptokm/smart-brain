70144705d7a7ab016660425839f9b8682c5c1f6a
sudwiptokm <94325115+sudwiptokm@users.noreply.github.com>
2021-12-03 20:26:51 +0600

    connect to Heroku API

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({imageUrl: this.state.input})
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://floating-shelf-50682.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -101,7 +101,7 @@ class App extends Component {
     .then(res => res.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://floating-shelf-50682.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component{
     }
 
     onSubmitSignIn = () => {
-        fetch("http://localhost:3000/register",{
+        fetch("https://floating-shelf-50682.herokuapp.com/register",{
             method: "post",
             headers: {"Content-Type" : "application/json"},
             body: JSON.stringify({
